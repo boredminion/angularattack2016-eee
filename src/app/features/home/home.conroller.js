09599bb6294bd0a737eca841fad2c876025fc489
divyanshumittal <phadithya@gmail.com>
2016-05-14 18:36:11 -0500

    master: push category and time selector

--- src/app/features/home/home.conroller.js
+++ src/app/features/home/home.conroller.js
@@ -9,6 +9,32 @@
     function HomeController(homeService) {
         var vm = this;
         vm.title = "Reading ninja home page";
+        vm.selectedCategories = [];
+        vm.timeValue = 30;
+        vm.categories = [{
+            name: 'Sports'
+        }, {
+            name: 'Nature'
+        }, {
+            name: 'Illinois'
+        }, {
+            name: 'International'
+        }];
+
+        vm.selectCategory = selectCategory;
+        vm.timeChanged = timeChanged;
+
+        function timeChanged() {
+            console.log(vm.timeValue);
+        }
+
+        function selectCategory(category) {
+            category.selected = true;
+            vm.selectedCategories.push(category);
+            
+        };
+        
+        
         var params = {
             'api-key': 'ed023a9c67d14448bc9069ea3bd3f8e5',
             q: 'sports'
