ea2e0c2114fae284a5f0674d1311f11411fec4c6
Vivek Mathew <jacob.vivek@gmail.com>
2016-05-15 12:43:26 -0500

    Adding twitter Share and save bookmark

--- src/app/features/home/home.controller.js
+++ src/app/features/home/home.controller.js
@@ -3,7 +3,45 @@
 
     angular
         .module('reading-ninja.home')
-        .controller('HomeController', HomeController);
+        .controller('HomeController', HomeController)
+        .directive('bookmarkPage', BookmarkPage);
+    
+
+    function BookmarkPage($window, $location){
+        return {
+            restrict: "AEC",
+            link: function (scope, element, attrs) {
+                $(element).click(function (e) {
+                    var bookmarkURL = this.href;
+                    var bookmarkTitle = document.title;
+                    var triggerDefault = false;
+
+                    if ($window.sidebar && $window.sidebar.addPanel) {
+                        // Firefox version < 23
+                        $window.sidebar.addPanel(bookmarkTitle, bookmarkURL, '');
+                    } else if (($window.sidebar && (navigator.userAgent.toLowerCase().indexOf('firefox') > -1)) || ($window.opera && $window.print)) {
+                        // Firefox version >= 23 and Opera Hotlist
+                        var $this = $(this);
+                        $this.attr('href', bookmarkURL);
+                        $this.attr('title', bookmarkTitle);
+                        $this.attr('rel', 'sidebar');
+                        $this.off(e);
+                        triggerDefault = true;
+                    } else if ($window.external && ('AddFavorite' in $window.external)) {
+                        // IE Favorite
+                        $window.external.AddFavorite(bookmarkURL, bookmarkTitle);
+                    } else {
+                        // WebKit - Safari/Chrome
+                        alert('Press ' + (navigator.userAgent.toLowerCase().indexOf('mac') != -1 ? 'Cmd' : 'Ctrl') + '+D to bookmark this page.');
+                        $window.external.AddFavorite(bookmarkURL, bookmarkTitle);
+                    }
+
+                    return triggerDefault;
+                });
+            }
+
+        }
+    }
 
     /** @ngInject */
     function HomeController(homeService, $filter, $window, $timeout, $state) {
